72216baa4ab8608239b6446063f05586c59d68d0
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-21 22:48:36 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("\nWelcome to the Shopping App!\n")
+
+user_name = input("Please enter your name:\n")
+print(f"\n\nHi {user_name}! Welcome to the Shopping App!\n")
+
+item1 = ['blouse', 89.99, 3]
+item2 = ['boots', 124.49, 1]
+item3 = ['ascot', 215, 2]
+item4 = ['trousers', 349, 5]
+item5 = ['scarf', 79.99, 10]
+
+print("Inventory")
+print("********************")
+print(item1)
+print(item2)
+print(item3)
+print(item4)
+print(item5)
+print("********************\n")
