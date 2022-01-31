bd9a2403f12dd71cc16cac969b7b1e673b101775
lusbyc <43541354+lusbyc@users.noreply.github.com>
2022-01-31 13:52:31 -0500

    added functionality for updating inventory quantity

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-
 def print_inventory(inventory):
+    print("{:^24}".format("Inventory"))
+    print("************************")
     for item in inventory:            
-        print(f"{item.name}  ${item.price}  {item.quantity}")
-        
+        print ('{:<10} {:<10} {:<10}'.format(f"{item.name}", f"${item.price}", f"{item.quantity}"))
+    print("************************")    
 
 def print_shopping_cart(shopping_cart):
     for i in shopping_cart:
@@ -27,8 +28,6 @@ item5 = Item('Scarf', 79.99, 10)
 
 inventory = [item1, item2, item3, item4, item5]
 
-print("Inventory")
-print("************")
 print_inventory(inventory)
 
 print()
@@ -41,44 +40,38 @@ def add_to_cart(item, cart, quantity):
 
 keep_going = 'y'
 while keep_going.lower() == 'y':
-    user_selection = input("Which item would you like to add to your shopping cart: ")
-    quantity = int(input("How many would you like? "))
+    user_selection = input("\nWhich item would you like to add to your shopping cart: ")
+    quantity = int(input("\nHow many would you like? "))
     for item in inventory:
-        if user_selection.lower() in item.name.lower():
+        if user_selection.lower() in item.name.lower() and quantity <= item.quantity:
             add_to_cart(item, cart, quantity)
 
-    keep_going = input("That's a great choice! Enter 'y' to add another item: ")
+    keep_going = input("\nThat's a great choice! Enter 'y' to add another item: ")
 else:
     print(f"\n\n{user_name} I bet you'll look amazing in those.\n\n")
 
-print("You've added the following items to your shopping cart")
-
+print(f"You've added the following items to your shopping cart")
 print_shopping_cart(cart)
 print()
 
 def remove_from_inventory(cart, inventory):
     for item in inventory:
         item.quantity = item.quantity - 1
-        # for i in cart:
-        #     if item in cart:
-        #         item.quantity = item.quantity - i.quantity
     return inventory
-
-
+    
 tax = 0
 
 def get_total(cart, quantity):
-    sum = 0
+    sum = 0.0
     for item in cart:
-        sum = sum + item.price(quantity)
+        sum += item.price * (quantity)
     tax = sum * .06
-    total = sum + tax
+    total = "${:,.2f}".format(sum + tax)
     return total
 
 total = get_total(cart, quantity)
 
-print(f"Your total with tax is ${total}.")
+print(f"Your total with tax is {total}.")
 print()
 
-# remove_from_inventory(cart, inventory)
 print_inventory(inventory)
